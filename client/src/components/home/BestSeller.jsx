122a1a9475270ccceff2d2e6beafb9151731a1fd
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 18:42:55 +0530

    ✅  Home Page

--- client/src/components/home/BestSeller.jsx
+++ client/src/components/home/BestSeller.jsx
@@ -6,12 +6,12 @@ const BestSeller = () => {
   const { products } = useContext(AppContext);
 
   return (
-    <div className="my-20 w-full">
+    <div className="my-20">
       <h2 className={`text-2xl md:text-3xl font-medium text-gray-800`}>
         Best Seller
       </h2>
 
-      <div className="my-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 items-center justify-center">
+      <div className="my-6 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {products
           .filter((product) => product.productInStock === true)
           .slice(0, 5)
