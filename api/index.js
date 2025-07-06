455cbd2f756b1e95b3ee24ea95cc696275de36a8
Ishaansyal595 <157666346+Ishaansyal595@users.noreply.github.com>
2025-07-06 20:04:49 +0530

    ✅   Fix Cors

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ const port = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "https://food-delivery-app-iota-rose.vercel.app",
+    origin: "https://food-delivery-app-chi-navy.vercel.app/",
     credentials: true,
   })
 );
